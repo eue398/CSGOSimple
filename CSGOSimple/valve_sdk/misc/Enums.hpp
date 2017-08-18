bd83958303abb1bed988e3fc53c45f2f2340bdd4
uwu <31023540+eue398@users.noreply.github.com>
2017-08-18 03:27:19 -0700

    Update Enums.hpp

--- CSGOSimple/valve_sdk/misc/Enums.hpp
+++ CSGOSimple/valve_sdk/misc/Enums.hpp
@@ -1,5 +1,39 @@
 #pragma once
 
+enum modtype_t {
+    mod_bad = 0,
+    mod_brush,
+    mod_sprite,
+    mod_studio,
+	MAXMODTYPE
+};
+
+enum Hitboxes {
+    HITBOX_HEAD,
+    HITBOX_NECK,
+    HITBOX_LOWER_NECK,
+    HITBOX_PELVIS,
+	HITBOX_BODY,
+	HITBOX_THORAX,
+    HITBOX_STOMACH,
+    HITBOX_LOWER_CHEST,
+    HITBOX_CHEST,
+    HITBOX_UPPER_CHEST,
+    HITBOX_RIGHT_THIGH,
+    HITBOX_LEFT_THIGH,
+    HITBOX_RIGHT_CALF,
+    HITBOX_LEFT_CALF,
+    HITBOX_RIGHT_FOOT,
+    HITBOX_LEFT_FOOT,
+    HITBOX_RIGHT_HAND,
+    HITBOX_LEFT_HAND,
+    HITBOX_RIGHT_UPPER_ARM,
+    HITBOX_RIGHT_FOREARM,
+    HITBOX_LEFT_UPPER_ARM,
+    HITBOX_LEFT_FOREARM,
+    MAXHITBOXES
+};
+
 enum CSWeaponType
 {
     WEAPONTYPE_KNIFE = 0,
